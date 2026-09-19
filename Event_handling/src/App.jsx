17b2b2fbd900d091b821d9ae0cd5a7810c1d33fa
Niyati <niyatijhalani15@gmail.com>
2026-09-19 16:15:41 +0530

    Updated

--- Event_handling/src/App.jsx
+++ Event_handling/src/App.jsx
@@ -1,121 +1,56 @@
 import { useState } from 'react'
-import heroImg from './assets/hero.png'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  function handleclick(){
+    alert("I am clicked"); 
+     
+  }
+  function mouse(){
+    alert("Para ke upar mouse leke aye ho"); 
+  }
+  function handleinputchange(e){
+    console.log("value till now: ", e.target.value);
+}
+function handlesubmit(e){
+  e.preventDefault();
+  //i am writing y custom behaviour down
+  alert("Form submit kardu kya");
+}
+
 
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+    <div>
+    {/* immediate invokation */}
+{/* <button onClick={alert("Button click hua h") }>
+  click me
+</button>  */}
 
-      <div className="ticks"></div>
+<button onClick={() => alert("Button is clicked")}>
+click me
+</button> 
+      {/* <form action="" onSubmit={handlesubmit}>
+         <input type="text" onChange={handleinputchange}/>
+     <button type='submit '>
+      Submit
+     </button>
+      </form> */}
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
 
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
+      {/* <p onMouseOver={mouse} style={{border:"1px solid black"}}>
+        i am para
+
+
+
+
+
+      </p>
+      <button onClick={handleclick} 
+      style={{border:"2px solid red"} }>
+        Click me 
+      </button> */}
+    </div>
   )
 }
 
